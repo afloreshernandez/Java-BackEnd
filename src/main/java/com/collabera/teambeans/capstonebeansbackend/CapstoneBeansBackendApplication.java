a44b396fb8678f2e32ae1f379ec7983fbcdb5821
Sternbery <stefansternberg@gmail.com>
2020-01-18 11:16:03 -0600

    First commit?

---
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/CapstoneBeansBackendApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.teambeans.capstonebeansbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBeansBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBeansBackendApplication.class, args);
+	}
+
+}
