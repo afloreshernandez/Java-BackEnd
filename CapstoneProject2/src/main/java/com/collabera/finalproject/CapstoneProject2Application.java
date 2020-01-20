d7adeed316c37e1321a52242ae3d7d5f40482208
Arianne Hernandez <40613380+afloreshernandez@users.noreply.github.com>
2020-01-19 18:11:19 -0600

    Add files via upload

---
+++ CapstoneProject2/src/main/java/com/collabera/finalproject/CapstoneProject2Application.java
@@ -0,0 +1,13 @@
+package com.collabera.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProject2Application.class, args);
+	}
+
+}
---
+++ CapstoneProject2/src/main/java/com/collabera/finalproject/blankclass.java
@@ -0,0 +1,5 @@
+package com.collabera.finalproject;
+
+public class blankclass {
+
+}
