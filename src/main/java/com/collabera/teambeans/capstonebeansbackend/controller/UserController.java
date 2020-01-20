96e52d85ccbf15b077c3d8988400b7660b886445
Stefan <stefansternberg@gmail.com>
2020-01-20 16:51:31 -0600

    made todo controllers crossorigin

--- src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/UserController.java
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +18,7 @@ import com.collabera.teambeans.capstonebeansbackend.model.UserDetails;
 import com.collabera.teambeans.capstonebeansbackend.repository.UserDetailsRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	@Autowired
