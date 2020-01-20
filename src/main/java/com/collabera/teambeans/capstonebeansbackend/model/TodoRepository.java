a7b4d7b2f4bb72e73195dd2f9a150b5bede8e609
Sternbery <stefansternberg@gmail.com>
2020-01-20 09:48:51 -0600

    Added two methods to TodoRepo

--- src/main/java/com/collabera/teambeans/capstonebeansbackend/model/TodoRepository.java
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/model/TodoRepository.java
@@ -1,9 +1,12 @@
 package com.collabera.teambeans.capstonebeansbackend.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoTask, Long>{
-	
+	List<TodoTask> findByUser(UserDetails user);
+	List<TodoTask> findByUser(long user_id);
 }
