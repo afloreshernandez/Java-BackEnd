1d570358517cb94150d5db0380d74f7e15bc9c5d
Stefan <stefansternberg@gmail.com>
2020-01-20 14:36:23 -0600

    added one to many annotated thing in userdetails

--- src/main/java/com/collabera/teambeans/capstonebeansbackend/model/UserDetails.java
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/model/UserDetails.java
@@ -1,10 +1,13 @@
 package com.collabera.teambeans.capstonebeansbackend.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -37,6 +40,9 @@ public class UserDetails {
 	@NotBlank
 	private String userRole;
 
+	@OneToMany
+	List<TodoTask> todotasks;
+	
 	public UserDetails() {
 		super();
 	}
