e6a2795edd12161c840b72b764394662860d69e9
Stefan <stefansternberg@gmail.com>
2020-01-21 15:24:42 -0600

    readded CrossOrigin for the umpteenth time

--- src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/TodoTaskController.java
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/TodoTaskController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import com.collabera.teambeans.capstonebeansbackend.repository.TodoRepository;
 import com.collabera.teambeans.capstonebeansbackend.repository.UserDetailsRepository;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoTaskController {
 
 	@Autowired
