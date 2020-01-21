a4c56b329ad0e2cf8763fa18ec4c40e77023f1c8
Alejandro <redmenace713@outlook.com>
2020-01-21 14:16:59 -0600

    removed duplicate code in POM. Added UserSecurity prototype

---
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/security/UserSecurity.java
@@ -0,0 +1,22 @@
+package com.collabera.teambeans.capstonebeansbackend.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class UserSecurity extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().
+        disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+}
