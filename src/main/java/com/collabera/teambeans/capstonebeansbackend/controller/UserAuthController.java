b782492e421a34ba7e82b900e396162e5e6c753b
Alejandro <redmenace713@outlook.com>
2020-01-22 15:13:13 -0600

    Removed Spring security dependency in POM. And comment out 3 classes

--- src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/UserAuthController.java
+++ src/main/java/com/collabera/teambeans/capstonebeansbackend/controller/UserAuthController.java
@@ -1,24 +1,24 @@
-package com.collabera.teambeans.capstonebeansbackend.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.collabera.teambeans.capstonebeansbackend.bean.UserAuthenticationBean;
-
-public class UserAuthController {
-
-@Deprecated	
-
-@CrossOrigin(origins = "http://localhost:4200")
-@RestController
-@RequestMapping("/api/v1")
-public class BasicAuthController {
-
-	@GetMapping(path = "/basicauth")
-	public UserAuthenticationBean basicauth() {
-			return new UserAuthenticationBean("You are authenticated");
-		}
-	}
-}
+//package com.collabera.teambeans.capstonebeansbackend.controller;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.collabera.teambeans.capstonebeansbackend.bean.UserAuthenticationBean;
+//
+//public class UserAuthController {
+//
+//@Deprecated	
+//
+//@CrossOrigin(origins = "http://localhost:4200")
+//@RestController
+//@RequestMapping("/api/v1")
+//public class BasicAuthController {
+//
+//	@GetMapping(path = "/basicauth")
+//	public UserAuthenticationBean basicauth() {
+//			return new UserAuthenticationBean("You are authenticated");
+//		}
+//	}
+//}
